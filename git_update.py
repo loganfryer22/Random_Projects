48582cad89da03725d54d710ea2de988271d9975
Logan Fryer <logan_fryer@outlook.com>
2025-02-07 19:55:21 -0700

    added git update test code

---
+++ git_update.py
@@ -0,0 +1,10 @@
+# this is to test updating code via git
+
+updated_version = "1.0"
+
+current_version = "1.0"
+
+if current_version == updated_version:
+    print("Version is update to date")
+else:
+    print("Version is NOT update to date")
